49b1511dab4f8caa13e935664bdf6521336b8de8
thankhonor <thankhonor@163.com>
2026-05-06 17:39:59 +0800

    5.6

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
 }
 
 export default nextConfig
